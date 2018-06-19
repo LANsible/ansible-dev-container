03d60be3d89e3bd5b1f117eff10f420eb16adf3e
wilmardo <info@wilmardenouden.nl>
2018-06-19 04:06:48 +0200

    fix(travis): Fix 2.6 build, typo in tags assignment

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-docker push lansible/ansible-dev-container
+docker push lansible/ansible-dev-container:$ANSIBLE_VERSION
