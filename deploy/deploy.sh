5c6c4cc442c7d7574c4604fdd9a878ec7ea301ae
wilmardo <info@wilmardenouden.nl>
2018-08-13 23:33:44 +0200

    Revert "fix(deply): Adds loop to push multiple tags again"
    This reverts commit 725079d7ef79d729ba1e53287a47193c86e2a523.

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-for tag in ${TAGS}
-do
-    echo "Pushing lansible/ansible-dev-container:${tag}"
-    echo "${DOCKER_PASSWORD}" | docker login -u "${DOCKER_USERNAME}" --password-stdin
-    docker push lansible/ansible-dev-container:${tag}
-done
+echo "Pushing lansible/ansible-dev-container:$ANSIBLE_VERSION"
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+docker push lansible/ansible-dev-container:$ANSIBLE_VERSION
