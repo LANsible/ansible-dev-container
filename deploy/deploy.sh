725079d7ef79d729ba1e53287a47193c86e2a523
wilmardo <info@wilmardenouden.nl>
2018-08-13 23:31:13 +0200

    fix(deply): Adds loop to push multiple tags again

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
-echo "Pushing lansible/ansible-dev-container:$ANSIBLE_VERSION"
-echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-docker push lansible/ansible-dev-container:$ANSIBLE_VERSION
+for tag in ${TAGS}
+do
+    echo "Pushing lansible/ansible-dev-container:${tag}"
+    echo "${DOCKER_PASSWORD}" | docker login -u "${DOCKER_USERNAME}" --password-stdin
+    docker push lansible/ansible-dev-container:${tag}
+done
