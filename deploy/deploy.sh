0868b6639d8377f7a1451b5be7d3657963e520f7
wilmardo <info@wilmardenouden.nl>
2018-08-13 23:34:34 +0200

    fix(deploy): Removes tag to push multiple tagged images

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-echo "Pushing lansible/ansible-dev-container:$ANSIBLE_VERSION"
-echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-docker push lansible/ansible-dev-container:$ANSIBLE_VERSION
+echo "Pushing lansible/ansible-dev-container:${ANSIBLE_VERSION}"
+echo "${DOCKER_PASSWORD}" | docker login -u "${DOCKER_USERNAME}" --password-stdin
+docker push lansible/ansible-dev-container
