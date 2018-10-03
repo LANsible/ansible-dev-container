c262b6029137643f244dceb745b4b576028f8d1c
wilmardo <info@wilmardenouden.nl>
2018-10-03 20:35:16 +0200

    fix(travis): Fix deployment and global env vars

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "Pushing ${DOCKER_NAMESPACE}/${CONTAINER_NAME}:${ANSIBLE_VERSION}"
 echo "${DOCKER_PASSWORD}" | docker login -u "${DOCKER_USERNAME}" --password-stdin
-docker push lansible/ansible-dev-container
+docker push ${DOCKER_NAMESPACE}/${CONTAINER_NAME}
